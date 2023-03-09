766da2492fa721e1291e94bf8ace14eed2218e09
Luis Sánchez <luis.alberto.sanchez.rios@live.com.mx>
2023-03-09 17:29:14 -0600

    start

--- Parcial2/P2T1-useContext/App.tsx
+++ Parcial2/P2T1-useContext/App.tsx
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './components/Hello';
+import ContextTest from './components/context/ContextTest';
+import RefTest from './components/RefTest/RefTest';
+import { User, UserContext } from './imports/userContext';
 
 export default function App() {
+  const [message, setMessage] = useState('Loading...');
+
+  const [user, setUser] = useState<User>({
+    id: 0,
+    name: 'Luis',
+    age: 22,
+    address: 'Some address.'
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage('Hola Mundo')
+      setUser({
+        ...user,
+        address: 'Conkal'
+      })
+    }, 2000);
+  }, [setMessage]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value={user}>
+      <View style={styles.container}>
+        <Text style={styles.sectionTitle}>{message}</Text>
+        {/* <Hello text={message} setText={setMessage}></Hello> */}
+        <ContextTest></ContextTest>
+        <RefTest/>
+        <StatusBar style="auto" />
+      </View>
+    </UserContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  sectionTitle: {
+    textAlign: 'center'
+  }
 });
---
+++ Parcial2/P2T1-useContext/components/Hello.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+interface HelloAttributes {
+  text: string,
+  setText?: Function
+};
+
+export default function Hello({ text, setText } : HelloAttributes) {
+
+  useEffect(() => {
+    setTimeout(() => {
+      setText && setText('Hola Mundo from Hello');
+    }, 5000);
+  }, [setText]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.sectionTitle}>{text}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  sectionTitle: {
+    textAlign: 'center'
+  }
+});
